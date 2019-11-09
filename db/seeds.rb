b28be727d40d6d5e1cbf11b4988ea0e59e98b6af
Liko <li_xie1992@hotmail.com>
2019-11-09 11:09:18 +0000

    added capability for clubs to be seeded, with correct relationships with countries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,17 @@
 require_relative 'data_generation/json_to_seed'
+# require 'pry'
 
 
 def run
-
-
-    # Country.destroy_all
-    # Position.destroy_all 
+    Country.destroy_all
+    Position.destroy_all 
+    Club.destroy_all
 
     data = run_json_to_seed
 
     countries = seed_countries(data)
     positions = seed_positions(data)
-
-    teams = seed_teams(data)
+    clubs = seed_clubs(data)
 
 end
 
@@ -21,6 +20,7 @@ def seed_countries(data)
     countries.each do |country|
         Country.create(name: country)    
     end
+    puts "Countries seeded"
 end
 
 def seed_positions(data)
@@ -28,9 +28,23 @@ def seed_positions(data)
     positions.each do |position|
         Position.create(name: position)
     end
+    puts "Positions seeded"
 end
 
-def seed_teams(data)
+def seed_clubs(data)
+    countries = data[:country_hash]
+    clubs = data[:club_array]
+    clubs.each do |club|
+        Club.create(
+            name: club[:name],
+            shorthand: club[:shorthand],
+            country_id: Country.find_by(name:club[:country]).id,
+            image: club[:image],
+            founded: club[:founded],
+            stadium_id: nil
+        )
+    end
+    puts "Clubs seeded"
 
 end
 
