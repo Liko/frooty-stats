bcb2750dd5e644671ae1f6cbad567497646c3c63
Tish Chungoora <tishal82@gmail.com>
2019-11-11 15:30:15 +0000

    Adding work-in-progress Favourites functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :players
   resources :clubs
+  resources :favourites
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/login' => 'sessions#new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_094952) do
+ActiveRecord::Schema.define(version: 2019_11_11_144047) do
 
   create_table "clubs", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2019_11_11_094952) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favourites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "club_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
