727659a18a5b2b8234d0b540fff513919f9352c3
github username <github email address>
2019-11-11 17:23:02 +0000

    added players index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :players
   resources :clubs
   resources :countries
-  resources :positions
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/login' => 'sessions#new'
