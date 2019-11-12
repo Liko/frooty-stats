74c35a682211124c74a99fee433dfe5383be9912
Tish Chungoora <tishal82@gmail.com>
2019-11-12 23:15:12 +0000

    Styling changes to new, edit and show view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
   
       @user = user
   
-      redirect_to controller: 'welcome', action: 'home'
+      redirect_to controller: 'home', action: 'today'
     end
   
     def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
 
-  root 'welcome#home'
+  root 'home#today'
 
 end
