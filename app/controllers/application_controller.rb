cbdd4adf98a118957edb958398c8661afb70a6f4
Tish Chungoora <tishal82@gmail.com>
2019-11-12 13:04:23 +0000

    Developed basic functionality for dynamic creation of favourite dropdown menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
     before_action :current_user
+    before_action :favourite_clubs
 
     def current_user
         @user = (User.find_by(id: session[:user_id]) || User.new)
@@ -12,6 +13,11 @@ class ApplicationController < ActionController::Base
 
     def require_logged_in
         return redirect_to(controller: 'sessions', action: 'new') unless logged_in?
+    end
+
+    def favourite_clubs
+        user_favs = current_user.favourites
+        @fav_clubs = Club.where(id: user_favs.map(&:club_id))
     end
 
 end
