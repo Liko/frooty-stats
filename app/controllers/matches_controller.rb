e39f09161b7efbfb9efe4454ef58f4f64d03f735
Liko <li_xie1992@hotmail.com>
2019-11-13 13:46:28 +0000

    added button to show full results, from competition show page

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,6 +3,8 @@ class MatchesController < ApplicationController
     def show
         @match = Match.find(params[:id])
 
+        @parent = @match
+
         @match_stats_array = [
             "possession", "shots", "shotsOnTarget", "corners", "offsides", "fouls", "yellow_cards", "red_cards"
         ]
