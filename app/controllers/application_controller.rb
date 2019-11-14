df12958050820e1c92df340703aed80c99d44ab3
Tish Chungoora <tishal82@gmail.com>
2019-11-14 11:31:09 +0000

    Declared logged_in? as a helper method in application_controller to use it in application.html.erb to render logged-in user specific tabs. This removes the need to use session[:user_id] in the logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
     before_action :current_user
     before_action :favourite_clubs
 
+    helper_method :logged_in?
+
     def current_user
         @user = (User.find_by(id: session[:user_id]) || User.new)
     end
