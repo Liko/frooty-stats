4ef0b508281ae5b72cf68891b2e4da6043edac96
Liko <li_xie1992@hotmail.com>
2019-11-14 13:25:22 +0000

    refactored club show page, partial for club_player_listings

--- app/models/position.rb
+++ app/models/position.rb
@@ -5,4 +5,8 @@ class Position < ApplicationRecord
         Position.all.map{|position|position.id}
     end
 
+    def self.get_pluralized_name(id)
+        self.find(id).name.pluralize
+    end
+
 end
