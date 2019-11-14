b2920d13360bd00f8b470bf651832d391424105c
Liko <li_xie1992@hotmail.com>
2019-11-14 15:18:31 +0000

    added upcoming fixtures/recent results for each favourite club, if user is logged in, else show upcoming fixtures. small home controller helper functions and also in match.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,18 @@
 class HomeController < ApplicationController
 
     def today
+        @favourites = favourite_clubs
+        @club_matches_array = []
+
+        @favourites.each do |club|
+            @club_matches_array << Match.getRelevantMatches(club) 
+        end
+
+        @competition_array  = @favourites.map{|club|club.competition}.uniq
+
+        num_upcoming_fixtures = 10
+        @upcoming_fixtures = Match.getUpcomingFixtures(num_upcoming_fixtures)
+
     end
 
 end
